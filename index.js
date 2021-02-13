35182fe6d2ce0a102d99a0c820a3e686bfefcc5f
John <vanya432134@gmail.com>
2021-02-13 18:35:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+// const path = require('path')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.status(200)
+    res.render('about')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
